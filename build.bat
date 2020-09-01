1d0094cb77a65379fddcae21a26a25c8c95677ce
Vlad-Stefan Harbuz <vlad@vladh.net>
2020-09-01 06:27:34 +0200

    Test some build flags

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set CommonCompilerFlags=/I "C:/opt/include/" -diagnostics:column -WL -Od -nologo -fp:fast -Zo -Oi -W4 -FC -Z7 -EHscc
+:: set CommonCompilerFlags=/I "C:/opt/include/" -diagnostics:column -WL -Od -nologo -fp:fast -Zo -Oi -W4 -FC -Z7 /D_ITERATOR_DEBUG_LEVEL=0
+set CommonCompilerFlags=/I "C:/opt/include/" -diagnostics:column -WL -nologo -fp:fast -Zo -Oi -W4 -FC -Z7 /D_ITERATOR_DEBUG_LEVEL=0
 set CommonLinkerFlags=/LIBPATH:"C:/opt/lib/" -STACK:0x100000,0x100000 -incremental:no -opt:ref /NODEFAULTLIB:msvcrt.lib opengl32.lib glfw3.lib assimp-vc142-mtd.lib user32.lib gdi32.lib shell32.lib
 
 pushd build
