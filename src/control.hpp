244fcb1937de5386db4074fca53fca7a0592e2dc
Vlad-Ștefan Harbuz <vlad@vladh.net>
2020-09-09 13:34:33 +0200

    Move keyboard stuff into control.cpp

--- src/control.hpp
+++ src/control.hpp
@@ -11,9 +11,18 @@ struct Control {
   real64 mouse_last_x;
   real64 mouse_last_y;
   real64 mouse_sensitivity;
+  bool32 key_states[1024];
+  bool32 prev_key_states[1024];
 };
 
 void control_init(Control *control);
-glm::vec2 control_update(Control *control, real64 mouse_x, real64 mouse_y);
+glm::vec2 control_update_mouse(Control *control, real64 mouse_x, real64 mouse_y);
+void control_update_keys(
+  Control *control, int key, int scancode, int action, int mods
+);
+bool32 control_is_key_down(Control *control, int key);
+bool32 control_is_key_up(Control *control, int key);
+bool32 control_is_key_now_down(Control *control, int key);
+bool32 control_is_key_now_up(Control *control, int key);
 
 #endif
