a47c85c6fa1d5ee2b0a24a0fcbac954828d6b457
Vlad-Stefan Harbuz <vlad@vladh.net>
2020-09-30 03:42:11 +0200

    Change from unordered_map to array. LUL

--- src/drawable_component_manager.cpp
+++ src/drawable_component_manager.cpp
@@ -14,9 +14,22 @@ DrawableComponent* DrawableComponentManager::add(
   new_component->entity_handle = entity_handle;
   new_component->model_asset = model_asset;
   new_component->target_render_pass = target_render_pass;
-  entity_handle_map[entity_handle] = new_component;
   return new_component;
 }
+
+
+DrawableComponent* DrawableComponentManager::get(EntityHandle handle) {
+  // NOTE: Normally we'd use a hash-map or something here, but
+  // std::unordered_map is slow as heck. This nice ol' array is faster.
+  // Let's look for something else if this starts showing up in the profiler.
+  for (uint32 idx = 0; idx < components->size; idx++) {
+    if (components->items[idx].entity_handle == handle) {
+      return &components->items[idx];
+    }
+  }
+  return nullptr;
+}
+
 
 void DrawableComponentManager::draw_all(
   SpatialComponentManager spatial_component_manager,
@@ -52,14 +65,5 @@ void DrawableComponentManager::draw_all(
         spatial ? &model_matrix : nullptr
       );
     }
-  }
-}
-
-
-DrawableComponent* DrawableComponentManager::get(EntityHandle handle) {
-  if (entity_handle_map.count(handle)) {
-    return entity_handle_map[handle];
-  } else {
-    return nullptr;
   }
 }
