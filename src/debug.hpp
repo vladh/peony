9cd4a0a8526cdb9e048683a9212f41b80812422d
Vlad-Stefan Harbuz <vlad@vladh.net>
2020-12-09 01:27:37 +0100

    Improve timing code

--- src/debug.hpp
+++ src/debug.hpp
@@ -7,9 +7,11 @@
   std::chrono::steady_clock::time_point debug_timerstart_##name = std::chrono::steady_clock::now();
 
 #define END_TIMER_MIN(name, min_duration_ms) \
-  std::chrono::duration<real64> debug_timerduration_##name = std::chrono::steady_clock::now() - debug_timerstart_##name; \
-  if (debug_timerduration_##name >= std::chrono::milliseconds(min_duration_ms)) { \
-    log_info("Timer %s took %f", #name, debug_timerduration_##name); \
+  std::chrono::duration<real64, std::milli> debug_timerduration_##name = ( \
+    std::chrono::steady_clock::now() - debug_timerstart_##name \
+  ); \
+  if (debug_timerduration_##name.count() >= min_duration_ms) { \
+    log_info("Timer %s took %f", #name, debug_timerduration_##name.count()); \
   }
 
 #define END_TIMER(name) END_TIMER_MIN(name, 0)
