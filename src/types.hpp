edf1431b44467ce9991ca23f1388aaba23ad7624
Vlad-Stefan Harbuz <vlad@vladh.net>
2021-05-03 14:28:24 +0200

    Make types a bit nicer

--- src/types.hpp
+++ src/types.hpp
@@ -9,31 +9,31 @@ typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
+typedef int8 s8;
+typedef int16 s16;
+typedef int32 s32;
+typedef int64 s64;
+
+typedef int8 bool8;
+typedef int16 bool16;
 typedef int32 bool32;
+typedef int64 bool64;
+typedef bool8 b8;
+typedef bool16 b16;
+typedef bool32 b32;
+typedef bool64 b64;
 
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
 typedef uint64_t uint64;
-
-typedef float real32;
-typedef double real64;
-
-typedef int8 s8;
-typedef int8 s08;
-typedef int16 s16;
-typedef int32 s32;
-typedef int64 s64;
-typedef bool32 b32;
-
 typedef uint8 u8;
-typedef uint8 u08;
 typedef uint16 u16;
 typedef uint32 u32;
 typedef uint64 u64;
 
-typedef real32 r32;
-typedef real64 r64;
+typedef float real32;
+typedef double real64;
 typedef real32 f32;
 typedef real64 f64;
 
