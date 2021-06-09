4a3a66966affe53d065acb400e6cb08e04ca53f5
Vlad-Stefan Harbuz <vlad@vladh.net>
2021-06-09 10:14:45 +0200

    Add stuff needed for clipping

--- src/glm.hpp
+++ src/glm.hpp
@@ -3,7 +3,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_access.hpp>
 #include <glm/gtx/quaternion.hpp>
 using glm::dot, glm::cross, glm::normalize, glm::abs, glm::max, glm::min,
   glm::ceil, glm::floor, glm::degrees, glm::radians, glm::transpose,
-  glm::inverse, glm::length, glm::length2;
+  glm::inverse, glm::length, glm::length2, glm::column, glm::row;
