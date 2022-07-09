cf2785745eea255617c442abad19160ecd1a1e9a
Vlad-Stefan Harbuz <vlad@vladh.net>
2022-07-09 22:22:54 +0100

    update copyright header

--- src/fonts.cpp
+++ src/fonts.cpp
@@ -1,8 +1,4 @@
-/*
-  Peony Game Engine
-  Copyright (C) 2020 Vlad-Stefan Harbuz <vlad@vladh.net>
-  All rights reserved.
-*/
+// (c) 2020 Vlad-Stefan Harbuz <vlad@vladh.net>
 
 #include "../src_external/glad/glad.h"
 #include "../src_external/pstr.h"
