16350c865e41a50d9fcffc77cd01bb2fc521054d
Vlad-Stefan Harbuz <vlad@vladh.net>
2022-07-15 19:23:43 +0100

    simplify Linux compile flags

--- make_linux.mk
+++ make_linux.mk
@@ -1,19 +1,14 @@
 # (c) 2020 Vlad-Stefan Harbuz <vlad@vladh.net>
 
 COMPILER_FLAGS = \
-	-I/usr/include/glm/include \
-	-I/usr/include/GLFW/include \
-	-I/usr/include/assimp/include \
-	-I/usr/include/freetype2 \
+	 $(shell pkg-config --cflags freetype2)
 	-D_FORTIFY_SOURCE=2 -ggdb3 -Og -Wall -Werror -Wextra -pedantic \
 	-std=c++2a \
 	-Wno-deprecated-volatile -Wno-unused-function -Wno-unknown-pragmas -Wno-comment \
 	-Wno-unused-parameter -Wno-sign-compare -Wno-missing-field-initializers \
 	-Wno-unused-result -Wno-class-memaccess -Wno-unused-but-set-variable
 
-LINKER_FLAGS = \
-	-L/usr/lib/x86_64-linux-gnu \
-	-lfreetype -lglfw -lassimp -lm -ldl -pthread
+LINKER_FLAGS = -lfreetype -lglfw -lassimp -lm -ldl -pthread
 
 .PHONY: unity run
 
